145467b62b339f5ca8309e1abcc11e2baf262cde
callmephilip <philip.nuzhnyy@gmail.com>
2013-05-24 12:03:27 -0700

    make require configuration reusable for runtime and testing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,15 @@
 /*
     Require setup + entry point for the application
+    This hybrid setup allows us to use 1 single config file across
+    both runtime and test environment  
 */
 
-require.config({
-    baseUrl : "scripts/",
+function testing(){
+    return typeof window.__karma__ !== 'undefined'; 
+}
+
+var requireConfig =  {
+    
     shim: {
         'underscore' : {exports: '_' },
         'backbone' : {exports: 'Backbone', deps: ['underscore']},
@@ -21,10 +27,33 @@ require.config({
         templates: '../templates'
     }
 
-});
+};
 
-require(['jquery','app'], function($,Application) {
-    if($("#mocha").length === 0){
+if (!testing()) {
+
+    requireConfig.baseUrl = "scripts/";
+    
+    require.config(requireConfig);
+
+    require(['jquery','app'], function($,Application) {
         Application.run();
+    });
+
+} else {
+
+    requireConfig.baseUrl = "/base/app/scripts";
+
+    var tests = [];
+    for (var file in window.__karma__.files) {
+        if (/specs\/.*\.js$/.test(file)) {
+            tests.push(file);
+        }
     }
-});
+
+    // ask Require.js to load these files (all our tests)
+    requireConfig.deps = tests;
+    // start test run, once Require.js is done
+    requireConfig.callback = window.__karma__.start;
+
+    require.config(requireConfig);
+}
