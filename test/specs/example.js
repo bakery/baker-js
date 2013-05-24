c7b5291e1da09941d7ebf8602e4cce4391dcf2b4
callmephilip <philip.nuzhnyy@gmail.com>
2013-05-24 12:04:20 -0700

    add a sample karma spec

---
+++ test/specs/example.js
@@ -0,0 +1,28 @@
+define(['jquery', 'underscore', 'backbone', 'app'], function($, _, Backbone, Application) {
+    
+
+    describe("accessing vendor libraries", function(){
+
+        it("can load jquery", function(){
+            expect($).toBeDefined();
+        });
+
+        it("can load underscore", function(){
+            expect(_).toBeDefined();
+        });
+
+        it("can load Backbone", function(){
+            expect(Backbone).toBeDefined();
+        });
+
+    });
+
+    describe("accessing application modules", function(){
+
+        it("can load application module", function(){
+            expect(Application).toBeDefined();
+        });
+
+    });
+
+});
