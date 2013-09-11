cc4a36d0af37823d56c70a42d74b940af11ff6e1
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2013-09-11 16:01:40 +0200

    lint the module

--- app/scripts/tools/marionette.override.js
+++ app/scripts/tools/marionette.override.js
@@ -1,4 +1,7 @@
-define(["underscore", "backbone","marionette","handlebars"],function(_,Backbone,Marionette,Handlebars){
+define(['underscore', 'backbone','marionette','handlebars'],function(_,Backbone,Marionette,Handlebars){
+    
+    'use strict';
+
     Backbone.Marionette.Renderer.render = function(template,data){
         return _.isFunction(template) ? template(data) : Handlebars.compile(template)(data);
     };
