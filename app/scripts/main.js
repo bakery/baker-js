8ef9f728741cd27ca350d2ede6498c0f27aad9a1
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2013-09-22 17:37:07 +0200

    update karma

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -44,6 +44,7 @@ if (!testing()){
         });
     });
 } else {
+
     var tests = [];
     for (var file in window.__karma__.files) {
         if (/specs\/.*\.js$/.test(file)) {
@@ -52,10 +53,13 @@ if (!testing()){
     }
 
     require.config({
+        // Karma serves files under /base, which is the basePath from your config file
         baseUrl : '/base/app/scripts',
-        // ask Require.js to load these files (all our tests)
-        deps : tests,
-        // start test run, once Require.js is done
-        callback : window.__karma__.start
+        
+        // dynamically load all test files
+        deps: tests,
+
+        // we have to kick of jasmine, as it is asynchronous
+        callback: window.__karma__.start
     });
 }
--- karma.conf.js
+++ karma.conf.js
@@ -1,67 +1,70 @@
 // Karma configuration
-// Generated on Thu May 23 2013 14:56:03 GMT-0700 (PDT)
+// Generated on Sun Sep 22 2013 17:07:42 GMT+0200 (CEST)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+    // frameworks to use
+    frameworks: ['jasmine', 'requirejs'],
 
 
-// list of files / patterns to load in the browser
-files = [
-    JASMINE,
-    JASMINE_ADAPTER,
-    REQUIRE,
-    REQUIRE_ADAPTER,
-
-    // these 2 patterns are for modules that are meant to be loaded by require
-    // main.js is excluded to enforce its inclusion later through a <script> tag
-    {pattern: 'app/scripts/**/!(main.js)', included: false},
-    {pattern: 'test/specs/**/*.js', included: false},
-
-    'app/scripts/main.js'
-];
-
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['progress'];
+    // list of files / patterns to load in the browser
+    files: [
+      'app/scripts/main.js',
+      {pattern: 'app/scripts/**/*.js', included: false},
+      {pattern: 'test/specs/**/*.js', included: false}
+    ],
 
 
-// web server port
-port = 9876;
+    // list of files to exclude
+    exclude: [
+      
+    ],
 
 
-// cli runner port
-runnerPort = 9100;
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress'],
 
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+    // web server port
+    port: 9876,
 
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
 
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['Chrome'],
 
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
+};
