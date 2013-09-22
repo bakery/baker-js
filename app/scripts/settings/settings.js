731fead56b4e972fde3df61f397df26aa2409daf
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2013-09-22 20:50:26 +0200

    rename dev settings to settings

--- app/scripts/settings/settings.js
+++ app/scripts/settings/settings.js
@@ -1,7 +1,7 @@
 define(['jquery','base.settings'], function($,BaseSettings){
 
-	'use strict';
-	
+    'use strict';
+    
     return $.extend(BaseSettings, {
         exampleString : 'this is overriden in settings/dev.js'
     });
